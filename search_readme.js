39d0989068e8c7d2fe882b3862e6e503c7894d64
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 11:45:58 +0100

    Update search_readme.js

--- search_readme.js
+++ search_readme.js
@@ -1,37 +1,35 @@
 
 
 async function search(word){
-	var found = false;
+	var count = 0;
+	var nameid = word + " not found";
+	var username = "";
+	var login = "";
 	
 	const response = await fetch('README.md');
 	const text = await response.text();
 	const textSplit = text.split("\n\n");
 	for(let i = 0; i < textSplit.length; i++){
 		if (textSplit[i].includes(word)){
-			if (found){
-				console.log("Multiple people found for " + word + ". Please refine search");
-				document.getElementById("nameid").innerHTML = "Multiple people found for " + word + ". Please refine search";
-				document.getElementById("username").innerHTML = "";
-				document.getElementById("login").innerHTML = "";
-				break;
-			}
-			
-			found = true;
+			count = count + 1;
 			var nameArr = textSplit[i].split("\n")
-			for(let j = 0; j < nameArr.length; j++){
-				console.log(nameArr[j]);
-			}
 			
-			document.getElementById("nameid").innerHTML = "Name: " + nameArr[0].split("\"")[3];
-			document.getElementById("username").innerHTML = "Username: " + nameArr[1].split("\"")[3];
-			document.getElementById("login").innerHTML = "Login: " + nameArr[2].split("\"")[3];
+			console.log("found " + nameArr[0].split("\"")[3]);
+			nameid = "Name: " + nameArr[0].split("\"")[3];
+			username = "Username: " + nameArr[1].split("\"")[3];
+			login = "Login: " + nameArr[2].split("\"")[3];
 		}
 		
 	}
-	if (!found){
-		console.log(word + " not found");
-		document.getElementById("nameid").innerHTML = word + " not found";
+	if (count > 1){
+		console.log("Multiple people found for " + word + ". Please refine search");
+		document.getElementById("nameid").innerHTML = "Multiple people found for " + word + ". Please refine search";
 		document.getElementById("username").innerHTML = "";
 		document.getElementById("login").innerHTML = "";
+	}
+	else{
+		document.getElementById("nameid").innerHTML = nameid;
+		document.getElementById("username").innerHTML = username;
+		document.getElementById("login").innerHTML = login;
 	}
 }
