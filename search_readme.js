57ac1fc683fa4a87b569dce3606930118970c2ce
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 10:05:06 +0100

    Fix using fetch API

--- search_readme.js
+++ search_readme.js
@@ -1,15 +1,7 @@
-const fs = require('fs');
-const readline = require('readline');
+
 
 async function search(){
-	const stream = fs.createReadStream('README.md');
 	
-	const inputlines = readline.createInterface({
-		input: stream,
-		crlfDelay: Infinity
-	});
+	fetch('README.md').then(response => response.md()).then(text => console.log(text))
 	
-	for await (const line of inputlines){
-		console.log(`Line from file: ${line}`);
-	}
 }
