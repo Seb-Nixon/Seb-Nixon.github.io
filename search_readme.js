a72d432dc9164d426ffb5951dc2fc034ecd4b6b5
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 14:45:58 +0100

    Update search_readme.js

--- search_readme.js
+++ search_readme.js
@@ -8,6 +8,14 @@ async function search(word){
 	var nameid = word + " not found";
 	var username = "";
 	var login = "";
+	var searchWord = "";
+	
+	if (word.includes("@newcastle.ac.uk")){
+		searchWord = word
+	}
+	else{
+		searchWord = word + "@newcastle.ac.uk"
+	}
 	
 	//Read the 'README.md' file and get the text.
 	const response = await fetch('JSON/names.json');
@@ -15,43 +23,20 @@ async function search(word){
 	const parseData = names;
 	edges = parseData.data.organization.samlIdentityProvider.externalIdentities.edges;
 	for (var i = 0; i < edges.length; i++){
-		console.log(edges[i].node.samlIdentity.nameId);
-	}
-	/*
-	//Split the text into each persons name username and login.
-	const textSplit = text.split("\n\n");
-	for(let i = 0; i < textSplit.length; i++){
-		//If the person is found.
-		if (textSplit[i].toUpperCase().includes(word.toUpperCase())){
-			count = count + 1;
-			var nameArr = textSplit[i].split("\n")
-			
-			//Log each person found by their nameid.
-			console.log("Found " + nameArr[0].split("\"")[3]);
-			
-			nameid = "Name: " + nameArr[0].split("\"")[3];
-			username = "Username: " + nameArr[1].split("\"")[3];
-			login = "Login: " + nameArr[2].split("\"")[3];
+		var person = edges[i].node;
+		if (person.samlIdentity.nameId == searchWord ||
+			person.scimIdentity.username == searchWord ||
+			person.user.login == word){
+				
+			document.getElementById("nameid").innerHTML = "University email: " + person.samlIdentity.nameId;
+			document.getElementById("username").innerHTML = "B number: " + person.scimIdentity.username;
+			document.getElementById("login").innerHTML = "Github username: " + person.user.login;
+			console.log("Found: " + edges[i].node.samlIdentity.nameId);
+			break;
 		}
-		
 	}
 	
-	
-	
-	
-	
-	//If multiple people were found under the search word.
-	if (count > 1){
-		console.log("Multiple people found for " + word + ". Please refine search");
-		document.getElementById("nameid").innerHTML = "Multiple people found for " + word + ". Please refine search";
-		document.getElementById("username").innerHTML = "";
-		document.getElementById("login").innerHTML = "";
-	}
-	//Show the person to the user.
-	else{
-		document.getElementById("nameid").innerHTML = nameid;
-		document.getElementById("username").innerHTML = username;
-		document.getElementById("login").innerHTML = login;
-	}
-	*/
+	document.getElementById("nameid").innerHTML = word + " not inside organization";
+    document.getElementById("username").innerHTML = "";
+	document.getElementById("login").innerHTML = "";
 }
