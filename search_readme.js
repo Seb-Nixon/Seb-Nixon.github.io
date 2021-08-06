21a5fdb8dd37be424aca468f939657e3011232f3
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 10:30:32 +0100

    Update search_readme.js

--- search_readme.js
+++ search_readme.js
@@ -2,11 +2,20 @@
 
 async function search(){
 	
+	const word = "Ralph.Luke@newcastle.ac.uk"
+	
 	const response = await fetch('README.md');
 	const text = await response.text();
 	const textSplit = text.split("\n\n");
 	for(let i = 0; i < textSplit.length; i++){
-		console.log(textSplit[i]);
+		if (textSplit[i].includes(word)){
+			var nameArr = textSplit[i].split("\n")
+			for(let j = 0; j < nameArr.length; j++){
+				console.log(nameArr[j]);
+			}
+			
+		}
+		
 	}
 	
 }
