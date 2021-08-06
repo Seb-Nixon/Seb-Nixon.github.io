a01f08641774df98ba159874d27d50b8bff0c825
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 10:22:34 +0100

    Update search_readme.js

--- search_readme.js
+++ search_readme.js
@@ -4,9 +4,9 @@ async function search(){
 	
 	const response = await fetch('README.md');
 	const text = await response.text();
-	for(let i = 0; i < text.length; i++){
-		console.log(text[i]);
+	const textSplit = text.split("\n");
+	for(let i = 0; i < textSplit.length; i++){
+		console.log(textSplit[i]);
 	}
-	
 	
 }
