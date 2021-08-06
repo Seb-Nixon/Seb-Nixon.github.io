a3b1f13e9c804a4c8e3036411364c08c108cb1d3
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 12:04:56 +0100

    Update search_readme.js

--- search_readme.js
+++ search_readme.js
@@ -1,15 +1,22 @@
 
-
+/**
+ * Function to search through 'README.md' file, searching for a given name.
+ * Once the person is found the nameid, username and login are presented to the user.
+ */
 async function search(word){
 	var count = 0;
 	var nameid = word + " not found";
 	var username = "";
 	var login = "";
 	
+	//Read the 'README.md' file and get the text.
 	const response = await fetch('README.md');
 	const text = await response.text();
+	
+	//Split the text into each persons name username and login.
 	const textSplit = text.split("\n\n");
 	for(let i = 0; i < textSplit.length; i++){
+		//If the person is found.
 		if (textSplit[i].includes(word)){
 			count = count + 1;
 			var nameArr = textSplit[i].split("\n")
@@ -21,12 +28,14 @@ async function search(word){
 		}
 		
 	}
+	//If multiple people were found under the search word.
 	if (count > 1){
 		console.log("Multiple people found for " + word + ". Please refine search");
 		document.getElementById("nameid").innerHTML = "Multiple people found for " + word + ". Please refine search";
 		document.getElementById("username").innerHTML = "";
 		document.getElementById("login").innerHTML = "";
 	}
+	//Show the person to the user.
 	else{
 		document.getElementById("nameid").innerHTML = nameid;
 		document.getElementById("username").innerHTML = username;
