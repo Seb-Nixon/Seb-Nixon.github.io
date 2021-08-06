739e203d8d4df8a18fdc3c474995bb8ce07d0154
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 11:05:24 +0100

    Present results if found

--- search_readme.js
+++ search_readme.js
@@ -3,12 +3,14 @@
 async function search(){
 	
 	const word = "Ralph.Luke@newcastle.ac.uk"
+	var found = false;
 	
 	const response = await fetch('README.md');
 	const text = await response.text();
 	const textSplit = text.split("\n\n");
 	for(let i = 0; i < textSplit.length; i++){
 		if (textSplit[i].includes(word)){
+			found = true;
 			var nameArr = textSplit[i].split("\n")
 			for(let j = 0; j < nameArr.length; j++){
 				console.log(nameArr[j]);
@@ -17,6 +19,8 @@ async function search(){
 		}
 		
 	}
-	console.log("Person not found");
-	document.getElementById("demo").innerHTML = "Person not found";
+	if (!found){
+		console.log("Person not found");
+		document.getElementById("demo").innerHTML = "Person not found";
+	}
 }
