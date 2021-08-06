d9160a2718a36e8ce7ddc14c1efa6ee71aa3e5c9
Seb-Nixon <b7012862@newcastle.ac.uk>
2021-08-06 10:19:22 +0100

    add for loop to see individual chars

--- search_readme.js
+++ search_readme.js
@@ -4,6 +4,9 @@ async function search(){
 	
 	const response = await fetch('README.md');
 	const text = await response.text();
-	console.log(text)
+	for(let i = 0; i < text.length; i++){
+		console.log(text[i]);
+	}
+	
 	
 }
